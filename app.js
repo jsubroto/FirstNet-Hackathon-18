41af647fca79b06f345f4eff002087605ea0661f
Jaimes Subroto <jaimes_subroto@yahoo.com>
2018-03-24 20:51:41 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var express = require('express'); // app server
+var bodyParser = require('body-parser'); // parser for post requests
+var watson = require('watson-developer-cloud'); // watson sdk
+var request = require('request');
+
+var app = express();
+
+// Bootstrap application settings
+app.use(express.static('./public')); // load UI from public folder
+app.use(bodyParser.json());
+
+// Create the service wrapper
+
+var conversation = new watson.ConversationV1({
+  // If unspecified here, the ASSISTANT_USERNAME and ASSISTANT_PASSWORD env properties will be checked
+  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+  username: process.env.ASSISTANT_USERNAME || '<username>',
+  password: process.env.ASSISTANT_PASSWORD || '<password>',
+  version_date: '2018-02-16'
+});
+
+app.get('/geo', function(req, res) {
+  var addr = req.query.location;
+  request.post(
+      'https://maps.googleapis.com/maps/api/geocode/json?address='+addr+'&key=<api_key>',
+      function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var jsonArr = JSON.parse(body);
+            var x=jsonArr.results;
+            var y=x[0];
+            var latitude = y.geometry.location.lat;
+            var longitude = y.geometry.location.lng;
+            var code = latitude + ' ' + longitude;
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.status(200).json({geoLocation: code});
+          }
+      }
+  );
+})
+
+// Endpoint to be call from the client side
+app.post('/api/message', function(req, res) {
+  var payload = {
+    workspace_id: '<workspace_id>',
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+  // Send the input to the conversation service
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    console.log(response.output.text);
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+require('dotenv').config({silent: true});
+var watson = require('watson-developer-cloud'); // watson sdk
+var vcapServices = require('vcap_services');
+var express = require('express'); // app server
+var app = express();
+
+var server = require('./app');
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
+server.listen(port, function() {
+  // eslint-disable-next-line
+  console.log('Server running on port: %d', port);
+});
+
+// speech to text token endpoint
+var sttAuthService = new watson.AuthorizationV1(
+  {
+      username: '<username>, 
+      password: '<password>', 
+      url : '<url>'
+  }
+);
+server.use('/api/speech-to-text/token', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  sttAuthService.getToken(
+    function(err, token) {
+      if (err) {
+        console.log('Error retrieving token: ', err);
+        res.status(500).send('Error retrieving token');
+        return;
+      }
+      res.send(token);
+    }
+  );
+});
+
+// text to speech token endpoint
+var ttsAuthService = new watson.AuthorizationV1(
+    {
+      username: '<username>',
+      password: '<password>', 
+      url: '<url>'
+    }
+);
+server.use('/api/text-to-speech/token', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  ttsAuthService.getToken(
+    function(err, token) {
+      if (err) {
+        console.log('Error retrieving token: ', err);
+        res.status(500).send('Error retrieving token');
+        return;
+      }
+      res.send(token);
+    }
+  );
+});
